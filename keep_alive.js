8fe9c9bccf6f2567adf6d998b8c08a3d87a2f3c4
tanitop1 <lucanataliet5@gmail.com>
2025-03-08 05:19:18 -0300

    Fix Keep_alive

--- keep_alive.js
+++ keep_alive.js
@@ -6,7 +6,11 @@ app.all('/', (req, res) => {
   res.send('Bot is running');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const keepAlive = () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+export { keepAlive };
